7c41080ffd12a09c8dd18445629e6478c71e5813
Ashutosh Mohapatra <ashutoshmohapatra2020@gmail.com>
2022-02-26 16:35:35 +0530

    redirect added

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -1,20 +1,18 @@
 import React, { useState } from 'react';
-import { FcGoogle } from 'react-icons/fc';
 import { FaFacebookF } from 'react-icons/fa';
 import { auth } from '../../firebase';
-import { onAuthStateChanged, signInWithPopup, FacebookAuthProvider,signOut } from 'firebase/auth';
+import { signInWithRedirect, FacebookAuthProvider,signOut } from 'firebase/auth';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { useAuthState } from "react-firebase-hooks/auth";
 
 const Login = () => {
-    // const [users, setUsers] = useState(null);
     const [user] = useAuthState(auth);
     const router = useRouter();
 
     const facebookProvider = new FacebookAuthProvider();
     const signInWithFacebook = () => {
-        signInWithPopup(auth, facebookProvider)
+        signInWithRedirect(auth, facebookProvider)
         .catch((err) => {
             router.push(`/error/${err}`)
         })
